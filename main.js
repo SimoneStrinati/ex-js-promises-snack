d6953274a9a415cd836962df0c3b75f0040356de
simonestrinati <simone.strinati@outlook.it>
2026-04-13 12:13:42 +0200

    Bonus snack 1

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Ottieni il titolo di un post con una Promise.
 
 // Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
 // 🎯 Bonus: Ottieni l'intero post con l'autore
 // Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
 
@@ -20,6 +21,28 @@ getPostTitle(1)
     .catch(err => console.error(err));
 
 
+/*Bonus: Ottieni l'intero post con l'autore*/
+
+function getPost(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(res => res.json())
+            .then(post => {
+                fetch(`https://dummyjson.com/users/${post.userId}`)
+                    .then(res => res.json())
+                    .then(user => {
+                        const result = { ...post, user }; // oppure si potrebbe usare post.user = user resolve(post), anche se modifica tutto l'oggetto originale "post".
+                        resolve(result);    
+                    })
+                    .catch(reject);
+            })
+            .catch(reject);
+    });
+};
+
+getPost(1)
+    .then(post => console.log(`L'intero post è:`, post))
+    .catch(err => console.error(err));  
 
 
 
